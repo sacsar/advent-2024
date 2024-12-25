2f624ee484ee6dd4e87b5b6bacac4fc99df1c9c1
Sebastian Csar <sacsar@gmail.com>
2024-12-24 16:43:34 -0800

    Finish day 3 part two

--- src/advent24/daythree.clj
+++ src/advent24/daythree.clj
@@ -2,19 +2,46 @@
   (:require [advent24.core :as core]))
 
 (def mul-instruction #"mul\((\d+),(\d+)\)")
+(def instruction #"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")
 
-(defn extract-instructions [line]
-  (re-seq mul-instruction line))
-
-(defn execute-instruction [[_, a, b]]
+(defn execute-mul-instruction [[_, a, b]]
   (let [n (Integer/parseInt a)
         m (Integer/parseInt b)]
     (* m n)))
 
-(defn -main [path]
-  (let  [lines (core/load-input identity path)
-         instructions (mapcat extract-instructions lines)]
+(defn extract-mul-instructions [line]
+  (re-seq mul-instruction line))
+
+(defn part-one [lines]
+  (let [instructions (mapcat extract-mul-instructions lines)]
     (->> instructions
-         (map execute-instruction)
+         (map execute-mul-instruction)
          (reduce + 0)
          println)))
+
+;; Part two: extend to support do and don't
+(defn parse-instruction [[m a b]]
+  (case m
+    "do()" :do
+    "don't()" :dont
+    ;; else it's a mul
+    (execute-mul-instruction [m a b])))
+
+(defn extract-instructions [line]
+  (map parse-instruction (re-seq instruction line)))
+
+(defn part-two [lines]
+  (let [instructions (mapcat extract-instructions lines)]
+    (:acc (reduce (fn [acc inst]
+                    (case inst
+                      :do (assoc acc :state :do)
+                      :dont (assoc acc :state :dont)
+                ;; default -- this is a mul
+                      (case (:state acc)
+                        :do (let [s (:acc acc)]
+                              (assoc acc :acc (+ s inst)))
+                        :dont acc))) {:state :do :acc 0} instructions))))
+
+(defn -main [path]
+  (let  [lines (core/load-input identity path)]
+    (println (part-two lines))))
